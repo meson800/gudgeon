5fec732dc4a41765fb8de0c6a15b774012b3c092
Christopher Johnstone <meson800@gmail.com>
2017-05-29 13:45:28 -0700

    Added more morse code capabilities

--- morse/morse_to_audio.py
+++ morse/morse_to_audio.py
@@ -7,21 +7,21 @@ import pyaudio
 
 
 p = pyaudio.PyAudio()
-def sine_tone(frequency, duration, volume=1, sample_rate=22050):
+# for paFloat32 sample values must be in range [-1.0, 1.0]
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=44100,
+                output=True)
+def sine_tone(frequency, duration, volume=1, sample_rate=44100):
     # generate samples, note conversion to float32 array
     samples = (np.sin(2*np.pi*np.arange(sample_rate*duration)*frequency/sample_rate)).astype(np.float32)
-
-    # for paFloat32 sample values must be in range [-1.0, 1.0]
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sample_rate,
-                    output=True)
-
+    #trim samples, such that it ends on a value that is near zero (so we don't get a weird clip at the end)
+    cut_samples = samples[:np.argwhere(abs(samples) < .01)[-1] + 1]
+    cut_samples[-1] = 0
     # play. May repeat with different volume values (if done interactively) 
     stream.write(volume*samples)
 
-    stream.stop_stream()
-    stream.close()
+
 
     
 
@@ -77,19 +77,23 @@ def encodeToMorse(message):
         
 #converts encoded morse code to tones
 def playMorseCode(message):
+    unit = .25
     for char in message[:]:
         if char == '.':
-            sine_tone(400,.3)
-            time.sleep(.1)
+            sine_tone(400,unit * 1)
+            time.sleep(unit * 1)
         if char == '-':
-            sine_tone(400, .6)
-            time.sleep(.1)
+            sine_tone(400, unit * 3)
+            time.sleep(unit * 1)
         if char == "/":
-            time.sleep(.7)
+            time.sleep(unit * 3)
         if char == ' ':
-            time.sleep(.6);
+            time.sleep(unit * 2);
 
-output = encodeToMorse("AC8TY test message")
+output = encodeToMorse("AC8TY dead drop at the place overlooking the stars")
+print(output)
 playMorseCode(output)
 
+stream.stop_stream()
+stream.close()
 p.terminate()
